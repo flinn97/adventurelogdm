70c33e655a53670333d658cb206a5f5fa5dbd96c
Taylor Davidson <taylormdavidson@gmail.com>
2024-08-15 14:41:57 -0600

    fixes for the last bug

--- src/view/pages/LogComponents/initLogComponent.js
+++ src/view/pages/LogComponents/initLogComponent.js
@@ -4,6 +4,7 @@ import ParentFormComponent from '../../../componentListNPM/componentForms/parent
 import MonsterMapItem from '../../monsterMapItem';
 import MapComponent from '../../../componentListNPM/mapTech/mapComponent';
 import MonsterMapItemSimplified from './monsterMapItemSimplified';
+import auth from '../../../services/auth';
 
 export default class InitiativeLogComponent extends Component {
   constructor(props) {
@@ -15,27 +16,31 @@ export default class InitiativeLogComponent extends Component {
 
   }
 
-  componentDidMount(){
+  async componentDidMount(){
+
     let app = this.props.app;
     let dispatch = app.dispatch
     let state = app.state;
-   state.componentList.sortSelectedList("participant","lastInit",true);
-   let obj = this.props.obj;
-   if (!obj.getJson().currentInit){
-    obj.setCompState({
-      currentInit: <MapComponent
-      filter={{search: obj.getJson().itemId, attribute: "encounterId"}}
-      app={app} name={"participant"}
-     cells={[
-       {custom:MonsterMapItemSimplified, props:{app:app,}},
-       ]
-     }
-     sectionStyle={{marginBottom:"2px", width:this.props.w, }}
-     cellStyle={{alignContent:"center", verticalAlign:"center", width:"fit-content", }}
-     theme={"logInitiative"}
-     />
-    });
-   }
+    await auth.firebaseGetter("participant", state.componentList, "type");
+    this.setState({start:true})
+
+  //  state.componentList.sortSelectedList("participant","lastInit",true);
+  //  let obj = this.props.obj;
+  //  if (!obj.getJson().currentInit){
+  //   obj.setCompState({
+  //     currentInit: <MapComponent
+  //     filter={{search: obj.getJson().itemId, attribute: "encounterId"}}
+  //     app={app} name={"participant"}
+  //    cells={[
+  //      {custom:MonsterMapItemSimplified, props:{app:app,}},
+  //      ]
+  //    }
+  //    sectionStyle={{marginBottom:"2px", width:this.props.w, }}
+  //    cellStyle={{alignContent:"center", verticalAlign:"center", width:"fit-content", }}
+  //    theme={"logInitiative"}
+  //    />
+  //   });
+  //  }
   }
  
 
@@ -48,8 +53,10 @@ export default class InitiativeLogComponent extends Component {
     let index = this.props.index;
     let phone = window.innerWidth > 800?false:true;
     let w = this.props.w;
-    let sorted = state.componentList.sortSelectedList("participant","lastInit",true);
-    const currentInit = obj.getJson().currentInit?obj.getJson().currentInit:<MapComponent
+    debugger
+    let sorted = state.componentList.sortSelectedList("participant","initiative",true);
+    let list = state.componentList.getList("participant", obj.getJson().itemId, "encounterId");
+    const currentInit = <MapComponent
     filter={{search: obj.getJson().itemId, attribute: "encounterId"}}
     app={app} name={"participant"}
    cells={[
@@ -69,6 +76,7 @@ export default class InitiativeLogComponent extends Component {
                   fontSize: styles.fonts.fontNormal,
                   alignContent:"flex-end",
       }}>
+        {this.state.start&&<>
         <div style={{color:styles.colors.color4, fontSize:phone?styles.fonts.fontSmall:styles.fonts.fontSmallest, marginTop:phone?"8px":"18px", marginBottom:phone?"12px":"8px"}}>
           Initiative Order
         </div>
@@ -77,6 +85,7 @@ export default class InitiativeLogComponent extends Component {
         currentInit
         }
        </div>
+       </>}
       </div>
       
     )
