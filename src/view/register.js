750ca9e2b6ec237dcb8277d215f76d9ecddc6c66
IsaacB <iwbcheetah@gmail.com>
2024-10-21 21:27:34 -0600

    google register Text

--- src/view/register.js
+++ src/view/register.js
@@ -166,8 +166,8 @@ export default class Register extends Component {
                                     paddingLeft: "14px", textDecoration: "underline 1px", textUnderlineOffset: "3px", textDecorationColor: styles.colors.color8,
                                     color: "white", fontSize: styles?.fonts?.fontSmall, alignContent: "center", alignSelf: "center", textDecoration: "",
                                     border: "solid 2px #4285F4F2", borderRadius: "11px"
-                                }}> <div style={{ display: "flex", flexDirection: "row", }}>
-                                        <img src={google} style={{ width: "24px", marginRight: "8px", height: "24px", marginTop: "2.5px" }} />
+                                }}> <div style={{ display: "flex", flexDirection: "row", justifyContent:"space-between" }}>
+                                        <img src={google} style={{ width: "24px", marginRight: "12px", height: "24px", marginTop: "2.5px" }} />
                                         <div style={{ background: "#4285F4F2", borderRadius: "0px 8px 8px 0px", padding: "4px 11px", fontSize: "18.2px", textDecoration: "" }}>Sign up with Google</div>
                                     </div>
 
