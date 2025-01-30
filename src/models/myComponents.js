cc819c6c3c8bd5b074dc2ba5b88b8705a0ed39bc
Taylor Davidson <taylormdavidson@gmail.com>
2025-01-30 09:32:31 -0700

    teaching

--- src/models/myComponents.js
+++ src/models/myComponents.js
@@ -876,34 +876,38 @@ class Compendium extends Campaign {
         super(opps);
         this.getPicSrc = this.getPicSrc.bind(this);
         this.setFormat = this.setFormat.bind(this);
+        this.get5e=this.get5e.bind(this);
     }
     json = {
         ...this.json,
         type: "compendium",
         format: "",
     }
-
-    setFormat(newVal){
-        if (newVal === "Statblock 5e"){
-            this.json = { ...this.json,
+    get5e(){
+        let json = { ...this.json,
                 attr1:"CR",
                 attr2:"Size",
                 attr3:"Type",
                 attr4:"Alignment",
                 attr5:"Environment",
                 format:"Statblock 5e"
-            }
-            this.setCompState({...this.json})
-        }else{
-            this.json = { ...this.json,
-                attr1:"",
-                attr2:"",
-                attr3:"",
-                attr4:"",
-                attr5:"",
-            }
-            this.setCompState({...this.json})
+            
         }
+        return json
+    }
+
+    setFormat(newVal){
+        let json = { ...this.json,
+            attr1:"",
+            attr2:"",
+            attr3:"",
+            attr4:"",
+            attr5:"",
+        }
+        if (newVal === "Statblock 5e"){
+            json = this.get5e();
+        }
+        this.json = json 
     }
 
     async getPicSrc(path) {
